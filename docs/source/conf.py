76050ab078a47478dfa6d38d9064e0bffa25d5a5
Marco Fagiolini <mfx@amdx.de>
2022-10-06 00:34:21 +0200

    Read version from package, include ghp extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,16 +6,20 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+from ccpublisher import __version__
+
 project = 'ccpublisher'
 copyright = '2022, Archimedes Exhibitions GmbH'
 author = 'Marco Fagiolini <mfx@amdx.de>'
-release = '1.0.0'
+release = __version__.__version__
+version = release
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx_rtd_theme',
+    'sphinx.ext.githubpages',
 ]
 
 templates_path = []
