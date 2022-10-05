f89b26a32a25c56ba65ba2f0bcd6b95c8a26195d
Marco Fagiolini <mfx@amdx.de>
2022-10-06 00:20:46 +0200

    Remove references to static and template paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ extensions = [
     'sphinx_rtd_theme',
 ]
 
-templates_path = ['_templates']
+templates_path = []
 exclude_patterns = []
 
 
@@ -27,4 +27,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = []
